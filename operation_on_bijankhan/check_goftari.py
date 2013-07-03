a6ee202ca8ce6fe1705833a1ab3d608b69f39d66
Kensuke Mitsuzawa <kensuke-mi@is.naist.jp>
2013-07-04 06:45:31 +0900

    commit check_normalization_error_in_preper.py

--- operation_on_bijankhan/check_goftari.py
+++ operation_on_bijankhan/check_goftari.py
@@ -16,6 +16,7 @@ def split_bijankhan_and_add_to_list(bijankhan_lines, bijankhan_wordset):
     DESCRIPTION:In the bijankhan corpus, each data is tab separated cf."word\tPOS". Thus, this function split each line by '\t'. Sometimes the number of elements in the splited list is more than 2. This is because words in bijankhan corpus is concatenated by normal space. In that case, this code copes with other functions 'exception_of_aplited_num_is_*'. These functions concatenate prefix and stem and suffix by semi-space(ZWNJ).
     """
     for bijankhan_line in bijankhan_lines:
+        bijankhan_line=bijankhan_line.strip(u'\n');
         bijankhan_line=post_preper.normalize_line_by_regularexp(bijankhan_line);
         if len(bijankhan_line.split())==3:
             bijankhan_word=exception_of_splited_num_is_3(bijankhan_line.split());
@@ -43,12 +44,12 @@ def main():
     bijankhan_wordset=[];
     folklore_wordset=[];
     bijankhan_lines=codecs.open(sys.argv[1], 'r', 'utf-8').readlines();
-    #folklore_lines=codecs.open(sys.argv[2], 'r', 'utf-8').readlines();
+    folklore_lines=codecs.open(sys.argv[2], 'r', 'utf-8').readlines();
     bijankhan_wordset=split_bijankhan_and_add_to_list(bijankhan_lines, bijankhan_wordset);
-    #folklore_wordset=split_folklore_and_add_to_list(folklore_lines, folklore_wordset);
-    #for folklore_word in folklore_wordset:
-    #    print folklore_word;
-    #compare_wordset_of_bijankhan_and_folklore(bijankhan_wordset, folklore_wordset);
+    folklore_wordset=split_folklore_and_add_to_list(folklore_lines, folklore_wordset);
+    for folklore_word in folklore_wordset:
+        print folklore_word;
+    compare_wordset_of_bijankhan_and_folklore(bijankhan_wordset, folklore_wordset);
 
 if __name__=='__main__':
     main();
--- operation_on_bijankhan/compare_operation.sh
+++ operation_on_bijankhan/compare_operation.sh
@@ -1,2 +1,4 @@
 suffix_normalized=".normalized"
+suffix_result=".exception_list"
 ruby pre_per2.rb $1 > $1$suffix_normalized
+python check_normalize_error_in_preper.py $1$suffix_normalized  $1$suffix_result
